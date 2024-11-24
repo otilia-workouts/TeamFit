58578544b00956a582267184a4d5e1db3eadfedf
Otilia Chelmuș <62885587+otiliach@users.noreply.github.com>
2024-11-25 00:38:41 +0200

    I changed RestController to Controller and ResponseBody.

--- team-fit/src/main/java/com/resourcemanagement/team_fit/controller/InitialController.java
+++ team-fit/src/main/java/com/resourcemanagement/team_fit/controller/InitialController.java
@@ -1,12 +1,13 @@
 package com.resourcemanagement.team_fit.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+@Controller
 public class InitialController {
     @GetMapping("/")
-    public String index() {
+    public @ResponseBody String index() {
         return "Web Development Project Team Fit ☻!";
     }
 }
