c7e2b2e101db9f1578a9a0d9a937a56c9c1ad06f
Otilia Chelmuș <62885587+otiliach@users.noreply.github.com>
2024-12-03 21:42:02 +0200

    Added CRUD operation on DB for Employee and Project.

--- team-fit/src/main/java/com/resourcemanagement/team_fit/controller/EmployeeController.java
+++ team-fit/src/main/java/com/resourcemanagement/team_fit/controller/EmployeeController.java
@@ -1,36 +1,64 @@
 package com.resourcemanagement.team_fit.controller;
 
+import com.resourcemanagement.team_fit.model.activity.Project;
 import com.resourcemanagement.team_fit.model.basic.Employee;
 import com.resourcemanagement.team_fit.model.basic.Person;
 import com.resourcemanagement.team_fit.service.ProjectService;
+import com.resourcemanagement.team_fit.service.SeedService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/teamfit")
 public class EmployeeController {
 
     private final ProjectService projectService;
+    private final SeedService seedService;
 
-    public EmployeeController(ProjectService projectService) {
+    public EmployeeController(ProjectService projectService, SeedService seedService) {
         this.projectService = projectService;
+        this.seedService = seedService;
     }
 
-    @GetMapping("/project/persons")
+    @GetMapping("/project/people")
     public @ResponseBody List<Person> getEmployees(){
-        return projectService.getEmployees();
+        return seedService.getEmployees();
     }
 
     @GetMapping("/project/person/{id}")
     public @ResponseBody Person getEmployee(@PathVariable(value = "id") String  employeeId) {
-        return projectService.getEmployeesById(employeeId);
+        return seedService.getEmployeesById(employeeId);
     }
 
     @PostMapping("/project/person")
     public @ResponseBody void addEmployee(@RequestBody Employee employee) {
-        projectService.addEmployee(employee);
+        seedService.addEmployee(employee);
     }
 
+    @PostMapping("/person/save")
+    public @ResponseBody void saveEmployee(@RequestBody Employee employee) {
+        projectService.savePerson(employee);
+    }
+
+    @PutMapping("/person/save/{id}")
+    public @ResponseBody Person updatePerson (@RequestBody Employee employee, @PathVariable(value = "id") Long  employeeId) {
+        return projectService.updatePerson(employee, employeeId);
+    }
+    @GetMapping("/person/{id}")
+    public @ResponseBody Person getEmployeeById(@PathVariable(value = "id") String  employeeId) {
+        return projectService.getPersonById(Long.valueOf(employeeId));
+    }
+
+    @GetMapping("/all-people")
+    public @ResponseBody List<Person> getAllPeople() {
+        return projectService.getAllPeople();
+    }
+
+    @DeleteMapping("/person/delete/{id}")
+    public @ResponseBody void deleteEmployee(@PathVariable(value = "id") String  employeeId) {
+        projectService.deletePerson(Long.valueOf(employeeId));
+    }
 }
--- team-fit/src/main/java/com/resourcemanagement/team_fit/controller/ProjectController.java
+++ team-fit/src/main/java/com/resourcemanagement/team_fit/controller/ProjectController.java
@@ -1,7 +1,10 @@
 package com.resourcemanagement.team_fit.controller;
 
 import com.resourcemanagement.team_fit.model.activity.Project;
+import com.resourcemanagement.team_fit.model.basic.Employee;
+import com.resourcemanagement.team_fit.model.basic.Person;
 import com.resourcemanagement.team_fit.service.ProjectService;
+import com.resourcemanagement.team_fit.service.SeedService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
@@ -11,25 +14,50 @@ import java.util.List;
 public class ProjectController {
 
     private final ProjectService projectService;
+    private final SeedService seedService;
 
-    public ProjectController(ProjectService projectService) {
+    public ProjectController(ProjectService projectService, SeedService seedService) {
         this.projectService = projectService;
+        this.seedService = seedService;
     }
 
     @GetMapping("/projects")
     public @ResponseBody List<Project> getProjects() {
-        return projectService.getProjects();
+        return seedService.getProjects();
     }
 
     @GetMapping("/project")
     public @ResponseBody Project getProject() {
-        return projectService.getProject();
+        return seedService.getProject();
     }
 
     @PostMapping("/project")
     public @ResponseBody void addProject(@RequestBody Project project) {
-        projectService.addProject(project);
+        seedService.addProject(project);
     }
 
+    @PostMapping("/project/save")
+    public @ResponseBody void saveProject(@RequestBody Project project) {
+        projectService.saveProject(project);
+    }
 
+    @PutMapping("/project/save/{id}")
+    public @ResponseBody Project updateProject (@RequestBody Project project, @PathVariable(value = "id") Long  projectId) {
+        return projectService.updateProject(project, projectId);
+    }
+
+    @GetMapping("/project/{id}")
+    public @ResponseBody Project getProjectById(@PathVariable(value = "id") String  projectId) {
+        return projectService.getProjectById(Long.valueOf(projectId));
+    }
+
+    @GetMapping("/all-projects")
+    public @ResponseBody List<Project> getAllProjects() {
+        return projectService.getAllProjects();
+    }
+
+    @DeleteMapping("/project/delete")
+    public @ResponseBody void deleteProject(@RequestBody Project project) {
+        projectService.deleteProject(project);
+    }
 }
